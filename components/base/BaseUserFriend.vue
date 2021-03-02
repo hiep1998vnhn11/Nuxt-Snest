20246f5af50a6f77a18ea4fd803df0e1d31890a3
hiep1998vnhn11 <hiep1998vnhn11@gmail.com>
2021-03-02 21:05:46 +0700

    add base component

---
+++ components/base/BaseUserFriend.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card class="rounded-lg mt-3">
+    <v-card-title class="font-weight-bold">
+      {{ $t('profile.Friends') }}
+      <v-card-subtitle>{{ user.friends_count }} friends</v-card-subtitle>
+      <v-spacer></v-spacer>
+      <v-btn text class="primary--text text-capitalize">
+        {{ $t('profile.More') }}
+      </v-btn>
+    </v-card-title>
+    <v-container class="mt-n10">
+      <v-row>
+        <v-col
+          v-for="friend in user.friends"
+          :key="`friend-${friend.friend_id}`"
+          cols="3"
+          class="text-center"
+        >
+          <router-link
+            :to="{
+              name: 'user-url',
+              params: { url: friend.user_friend.url }
+            }"
+          >
+            <v-avatar size="93" class="rounded-lg avatar-outlined">
+              <v-img
+                width="93"
+                :src="friend.user_friend.profile_photo_path"
+              ></v-img>
+            </v-avatar>
+          </router-link>
+          <router-link
+            :to="{
+              name: 'user-url',
+              params: { url: friend.user_friend.url }
+            }"
+            style="text-decoration: none; color: black;"
+            class="font-weight-bold"
+          >
+            {{ friend.user_friend.name }}
+          </router-link>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: ['user']
+}
+</script>
+
+<style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -13,6 +13,8 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex'
+import axios from 'axios'
 export default {
   data() {
     return {
@@ -34,8 +36,59 @@ export default {
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Vuetify.js',
+      loading: false,
+      error: null,
+      loadingMessageCard: false
     }
+  },
+  computed: {
+    ...mapGetters('user', ['currentUser']),
+    ...mapGetters('socket', ['socket']),
+    ...mapGetters('message', ['thresh']),
+    messageIcon() {
+      return this.$route.name === 'Home'
+        ? 'mdi-pencil-circle-outline'
+        : 'mdi-account-multiple-outline'
+    },
+    ...mapGetters('message', ['messageCards'])
+  },
+  methods: {
+    ...mapActions('user', ['getUser']),
+    ...mapActions('message', [
+      'closeMessageCard',
+      'newMessage',
+      'setThreshCard'
+    ]),
+    ...mapActions('socket', ['connectSocket']),
+    async fetchUser() {
+      this.error = null
+      this.loading = true
+      try {
+        await this.getUser()
+      } catch (err) {
+        this.error = err.toString()
+      }
+      this.loading = false
+    },
+    async onOpenMessage(user) {
+      this.loadingMessageCard = true
+      try {
+        const response = await axios.post(`/v1/user/thresh/${user.id}/get`)
+        if (response.data.data) {
+          this.thresh = response.data.data
+        } else {
+          this.thresh = { user_with: user }
+        }
+      } catch (err) {
+        this.error = err.response.data.message
+      }
+      this.loadingMessageCard = false
+    }
+  },
+  async created() {
+    if (!this.currentUser) await this.fetchUser()
+    // if (!this.socket || this.socket.disconnected) this.connectSocket()
   }
 }
 </script>
